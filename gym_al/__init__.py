417c9dcbf4641ac4ebfa783192986f6441e11761
lixiaopu <lixiaopu1994@gmail.com>
2019-12-04 14:50:37 +0200

    modify gym env

--- gym_al/__init__.py
+++ gym_al/__init__.py
@@ -2,7 +2,7 @@ from gym.envs.registration import register
 
 register(
     id='CartPoleAl-v2',
-    entry_point='gym.envs.classic_control:CartPoleEnv',
+    entry_point='gym_al.envs.cartpole_env:CartPoleEnvAl',
     max_episode_steps=200,
     reward_threshold=195.0,
 )
